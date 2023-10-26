2cec11effb7d33965384534901d09125331214f3
nagababunarise <nagababunarise123@gmail.com>
2023-10-26 05:58:23 +0000

    counter

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+class Welcome extends Component {
+  render() {
+    const {name} = this.props
+    return (
+      <div>
+        <h1>Welcome,{name}</h1>
+      </div>
+    )
+  }
+}
+
+export default Welcome
